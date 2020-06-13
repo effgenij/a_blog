3f0676b9d3099ec44e45e3cec89a3c07c7a03db7
effgenij <effgenij@gmail.com>
2020-06-13 23:07:24 +0300

    Added associations to user-articles models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-  validates :username, presence: true, 
-                        uniqueness: { case_sensitive: false }, 
+  has_many :articles
+
+  validates :username, presence: true,
+                        uniqueness: { case_sensitive: false },
                         length: { minimum: 3, maximum: 25 }
-  validates :email, presence: true, 
-                    uniqueness: { case_sensitive: false }, 
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
                     length: { maximum: 105 },
                     format: { with: URI::MailTo::EMAIL_REGEXP }
 end
