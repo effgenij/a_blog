dc43ab68c0ac0e0f94d61172605229fe014ad035
effgenij <effgenij@gmail.com>
2020-06-26 21:19:01 +0300

    Added before_action method and logged in user when user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: %i[show edit update]
   def show
-    @user = User.find(params[:id])
     @pagy, @articles = pagy(@user.articles, items: 5)
   end
 
@@ -14,12 +13,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to @user
@@ -31,6 +28,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
@@ -40,6 +38,9 @@ class UsersController < ApplicationController
 
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
